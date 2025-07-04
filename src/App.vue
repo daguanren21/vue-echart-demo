bbf0054afc1c8d2c891be8ee4077d34c6a123dd7
daguanren21 <1363917353@qq.com>
2025-07-04 14:56:26 +0800

    feat:  demo

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,309 @@
 <script setup lang="ts">
 import { useTemplateRef, onMounted, nextTick } from "vue";
 import * as echarts from "echarts";
-const echartRef = useTemplateRef<HTMLElement>('echart');
-onMounted(() => {
-    const instance = echarts.init(echartRef.value);
-    const option: echarts.EChartsOption = {
-      title: {
-        text: "美国整体3个工作日内发货达成率",
+const echartRef = useTemplateRef<HTMLElement>("echart");
+
+async function loadEchart(){
+await nextTick()
+const instance = echarts.init(echartRef.value);
+const option: echarts.EChartsOption = {
+  title: {
+    text: "美国整体3个工作日内发货达成率",
+    left: "40%",
+  },
+  grid: {
+    left: 50,
+    right: "4%",
+    bottom: 150,
+    //containLabel: true
+  },
+  yAxis: {
+    type: "value",
+    interval: 4,
+    min: 85,
+    max: 101,
+    splitLine: {
+      show: false,
+    },
+    axisLabel: {
+      formatter: "{value}%", // 格式化为百分比
+    },
+  },
+  xAxis: [
+    {
+      type: "category",
+      data: [
+        "4.12-4.18",
+        "4.19-4.25",
+        "4.25-5.2",
+        "5.3-5.9",
+        "5.10-5.16",
+        "5.17-5.23",
+        "5.24-5.30",
+        "5.31-6.6",
+        "4.12-4.18",
+        "4.19-4.25",
+        "4.25-5.2",
+        "5.3-5.9",
+        "5.10-5.16",
+        "5.17-5.23",
+        "5.24-5.30",
+        "5.31-6.6",
+        "4.12-4.18",
+        "4.19-4.25",
+        "4.25-5.2",
+        "5.3-5.9",
+        "5.10-5.16",
+        "5.17-5.23",
+        "5.24-5.30",
+        "5.31-6.6",
+        "4.12-4.18",
+        "4.19-4.25",
+        "4.25-5.2",
+        "5.3-5.9",
+        "5.10-5.16",
+        "5.17-5.23",
+        "5.24-5.30",
+        "5.31-6.6",
+        "4.12-4.18",
+        "4.19-4.25",
+        "4.25-5.2",
+        "5.3-5.9",
+        "5.10-5.16",
+        "5.17-5.23",
+        "5.24-5.30",
+        "5.31-6.6",
+      ],
+      axisLabel: {
+        rotate: 90, // 旋转 X 轴标签 90 度
+        interval: 0, // 每个标签都显示
+        margin: 10, // 调整标签和轴之间的距离
       },
-      tooltip: {
-        trigger: "axis",
+      splitLine: {
+        show: true,
       },
-      legend: {
-        data: ["Ceva", "Estes", "GIGA", "Metro"],
-        textStyle: {
-          color: "#000",
+      boundaryGap: true,
+      axisTick: {
+        show: true,
+        alignWithLabel: false,
+        interval: 0,
+      },
+    },
+    {
+      type: "category",
+      position: "bottom",
+      boundaryGap: true,
+      data: [],
+      min: 0,
+      max: 11,
+      axisTick: {
+        show: true,
+        interval: function (param) {
+          return param % 4 === 0 || !param;
         },
+        length: 110,
+        alignWithLabel: false,
       },
-      grid: {
-        left: "3%",
-        right: "4%",
-        bottom: "3%",
-        containLabel: true,
+    },
+  ],
+  series: [
+    {
+      type: "line",
+      data: [99, 96, 97.3, 99, 98, 94, 93, 92],
+      label: {
+        show: true,
+        position: "top",
+        formatter: "{c}%", // 格式化为百分比
       },
-      xAxis: {
-        type: "category",
-        boundaryGap: false,
+      markLine: {
+        lineStyle: { width: 2 }, // 标线大小
+        symbol: "none", //去掉线最后面的箭头,
         data: [
-          "01/01",
-          "02/01",
-          "03/01",
-          "04/01",
-          "05/01",
-          "06/01",
-          "07/01",
-          "08/01",
-          "09/01",
-          "10/01",
+          {
+            name: "水平线1",
+            yAxis: 96, // Y 值为给定值的标记线，用户自定义水平标线的值
+            lineStyle: { color: "green" },
+            symbolRotate: 1, // 起点标记的旋转角度
+            symbolSize: [25, 25], // 起点标记的大小
+          },
         ],
       },
-      yAxis: {
-        type: "value",
-        axisLabel: {
-          formatter: "{value}%",
-        },
-        splitLine: {
-          show: false,
-        },
-      },
-      series: [
-        {
-          name: "Ceva",
-          type: "line",
-          stack: "总量",
-          data: [100, 99.8, 99.9, 99.7, 99.9, 99.5, 99.6, 99.3, 99.4, 99.5],
-          smooth: true,
-          lineStyle: {
-            color: "#7F7FF7",
-          },
-          symbol: "circle",
-          symbolSize: 8,
-          itemStyle: {
-            color: "#7F7FF7",
-          },
-        },
-        {
-          name: "Estes",
-          type: "line",
-          stack: "总量",
-          data: [98.6, 99.0, 98.9, 99.1, 99.4, 99.0, 99.2, 99.3, 99.1, 99.2],
-          smooth: true,
-          lineStyle: {
-            color: "#5F9BFF",
-          },
-          symbol: "circle",
-          symbolSize: 8,
-          itemStyle: {
-            color: "#5F9BFF",
-          },
-        },
-        {
-          name: "GIGA",
-          type: "line",
-          stack: "总量",
-          data: [97.5, 98.0, 98.2, 98.5, 98.9, 99.0, 98.5, 98.7, 98.3, 98.6],
-          smooth: true,
-          lineStyle: {
-            color: "#4D9C99",
-          },
-          symbol: "circle",
-          symbolSize: 8,
-          itemStyle: {
-            color: "#4D9C99",
-          },
-        },
-        {
-          name: "Metro",
-          type: "line",
-          stack: "总量",
-          data: [96.3, 97.0, 96.5, 96.9, 97.2, 96.8, 97.1, 97.4, 97.0, 97.3],
-          smooth: true,
-          lineStyle: {
-            color: "#B88BFF",
-          },
-          symbol: "circle",
-          symbolSize: 8,
-          itemStyle: {
-            color: "#B88BFF",
-          },
-        },
+    },
+    {
+      type: "line",
+      smooth: true,
+      data: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        99,
+        96,
+        97.3,
+        99,
+        98,
+        94,
+        93,
+        92,
       ],
-    };
-    instance.setOption(option);
-});
+    },
+    {
+      type: "line",
+      smooth: true,
+      data: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        99,
+        96,
+        97.3,
+        99,
+        98,
+        94,
+        93,
+        92,
+      ],
+    },
+    {
+      type: "line",
+      smooth: true,
+      data: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        99,
+        96,
+        97.3,
+        99,
+        98,
+        94,
+        93,
+        92,
+      ],
+    },
+    {
+      type: "line",
+      smooth: true,
+      data: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        99,
+        96,
+        97.3,
+        99,
+        98,
+        94,
+        93,
+        92,
+      ],
+    },
+    {
+      type: "custom",
+      //@ts-expect-error
+      renderItem: function (params, api) {
+        debugger;
+        var valX = api.value(0);
+        var pointA = api.coord([valX, 0]);
+        //@ts-expect-error
+        var pointB = api.coord([valX - 1, 0]);
+
+        return {
+          type: "text",
+          style: {
+            text: api.value(2),
+            x: (pointA[0] + pointB[0]) / 2,
+            y: 320, //距上方距离
+            fill: "#333",
+            fontSize: 14,
+            textAlign: "center",
+          },
+        };
+      },
+      data: [
+        [4, 0, "整体"],
+        [12, 0, "Ceva"],
+        [20, 0, "Estes"],
+        [28, 0, "GIGA"],
+        [36, 0, "Metro"],
+      ],
+    },
+  ],
+};
+instance.setOption(option);
+}
+loadEchart()
 </script>
 
 <template>
